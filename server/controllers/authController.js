799f97f3ab1a2774238e9b752f553663ae5f25ff
pawan-vanam <129752046+pawan-vanam@users.noreply.github.com>
2025-12-10 20:01:43 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -278,8 +278,8 @@ exports.logout = async (req, res, next) => {
   res.cookie('token', 'none', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
-    secure: true, // Force this to true for production
-    sameSite: 'None', // Force this to 'None' for cross-origin
+    secure: true, // Required for HTTPS
+    sameSite: 'None', // Required for Cross-Site (Netlify->Render)
   });
 
   res.status(200).json({ success: true, message: 'User logged out' });
