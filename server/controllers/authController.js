9a28b789080320f306d68527f1a8d4a29cc9cccf
pawan-vanam <129752046+pawan-vanam@users.noreply.github.com>
2025-12-10 19:43:11 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -278,6 +278,8 @@ exports.logout = async (req, res, next) => {
   res.cookie('token', 'none', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // Add this
+    samesite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Add this
   });
 
   res.status(200).json({ success: true, message: 'User logged out' });
